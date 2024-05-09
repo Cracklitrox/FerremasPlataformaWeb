5d209b9362b5fad1d5caeda738e063ffe9b088dd
Carlos <c.gacitua@duocuc.cl>
2024-05-09 02:58:15 -0400

    Clases creadas: Suscripcion, TipoComunicacion, Producto, ProductoCarrito, Carrito y Pedido
    Todos los modelos creados hasta el momento.
    
    TipoComunicacion y Producto tiene su propia carpeta 'media' donde se guarda las imagenes descritas por el año, mes y dia de subido el archivo.

--- ferremas_market/Pedidos/admin.py
+++ ferremas_market/Pedidos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Producto)
+admin.site.register(ProductoCarrito)
+admin.site.register(Carrito)
+admin.site.register(Pedido)
--- ferremas_market/Pedidos/models.py
+++ ferremas_market/Pedidos/models.py
@@ -1,26 +1,58 @@
 from django.db import models
-# from ..Usuario.models import Cliente
+from Usuario.models import Cliente
+from django.utils import timezone
 
-# # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=120)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    foto = models.ImageField(upload_to='producto/%Y/%m/%d/', blank=True)
 
-# class TipoComunicacion(models.Model):
-#     descripcion = models.CharField(max_length=120)
-#     foto = models.ImageField(blank=True)
 
-# class Suscripcion(models.Model):
-#     cliente = models.OneToOneField(
-#         Cliente,
-#         on_delete=models.CASCADE,
-#         primary_key=True,
-#         related_name='suscripcion'  # Facilita el acceso desde el cliente a su suscripción
-#     )
-#     tipo_comunicacion = models.ForeignKey(
-#         TipoComunicacion,
-#         on_delete=models.CASCADE,
-#         related_name='suscripciones'  # Permite acceder a todas las suscripciones de un tipo específico
-#     )
-#     activo = models.BooleanField(default=True)
-#     fecha_registro = models.DateTimeField(auto_now_add=True)
+class Carrito(models.Model):
+    run = models.OneToOneField(
+        Cliente,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='pedido'
+    )
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    hora_actualizado = models.DateTimeField(auto_now=True)
+    activo = models.BooleanField(default=True)
 
-#     def __str__(self):
-#         return f"{self.cliente.run} - {self.tipo_comunicacion.descripcion}"
+class ProductoCarrito(models.Model):
+    carrito = models.ForeignKey(
+        Carrito,
+        on_delete=models.CASCADE,
+        related_name='productos_carrito'
+    )
+    producto = models.ForeignKey(
+        Producto,
+        on_delete=models.CASCADE,
+        related_name='productos_carrito'
+    )
+    cantidad = models.IntegerField()
+
+class Pedido(models.Model):
+    ESTADO_OPCIONES = (
+        (1, 'Pendiente'),
+        (2, 'En proceso'),
+        (3, 'Enviado'),
+        (4, 'Entregado')
+    )
+    run = models.ForeignKey(
+        Cliente,
+        on_delete=models.CASCADE,
+        related_name='pedidos'
+    )
+    estado = models.IntegerField(choices=ESTADO_OPCIONES, default=1)
+    informacion_adicional = models.CharField(max_length=120)
+    fecha_recivo = models.DateTimeField(auto_now_add=True)
+    fecha_envio = models.DateTimeField()
+    activo = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.estado == 3 and not self.fecha_envio:  # Estado 3 = 'Enviado'
+            self.fecha_envio = timezone.now()
+        super(Pedido, self).save(*args, **kwargs)
